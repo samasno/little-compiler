19602a7abd5cf228e77fdc187d343acd111565df
samasno <emmanuel@dv-analytics.io>
2024-02-15 12:31:28 -0600

    repl tokenizes

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -5,23 +5,33 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/samasno/little-compiler/pkg/lexer"
 )
 
 func Run() {
 	scanner := bufio.NewScanner(os.Stdin)
-	prompt()
-loop:
-	for scanner.Scan() {
-		text := scanner.Text()
-		text = strings.Replace(text, "\n", "", -1)
-		switch text {
-		case "quit":
-			println("received quit command")
-			break loop
-		default:
-			println(text)
-		}
+outer:
+	for {
 		prompt()
+	inner:
+		for scanner.Scan() {
+			text := scanner.Text()
+			text = strings.Replace(text, "\n", "", -1)
+			switch text {
+			case "quit":
+				println("received quit command")
+				break outer
+			default:
+				l := lexer.NewLexer(text)
+				ts := l.Tokenize()
+				for _, t := range ts {
+					fmt.Printf("%v\n", t)
+				}
+				break inner
+			}
+
+		}
 	}
 	println("exiting repl")
 }
