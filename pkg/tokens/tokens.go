9cca276bac9de493a69475e4f3159e14f979db03
samasno <emmanuel@dv-analytics.io>
2024-02-15 10:30:16 -0600

    token variables

---
+++ pkg/tokens/tokens.go
@@ -0,0 +1,46 @@
+package tokens
+
+type Token struct {
+	Type    string
+	Literal string
+}
+
+const (
+	IDENTIFIER = "IDENTIFIER"
+	ILLEGAL    = "ILLEGAL"
+	EOF        = ""
+
+	SPACE     = ` `
+	COMMA     = ","
+	SEMICOLON = ";"
+	COLON     = ":"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	LBRACKET  = "["
+	RBRACKET  = "]"
+	NEWLINE   = "\n"
+	TAB       = "\t"
+
+	// OPERATORS
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	MULTIPLY = "*"
+	DIVIDE   = "/"
+
+	// COMPARATORS
+	EQUALTO = "=="
+	LTE     = "<="
+	GTE     = ">="
+	NOT     = "!"
+
+	// KEYWORDS
+	STRING   = "str"
+	INT      = "int" // type declarations
+	INTEGER  = "INTEGER"
+	FUNCTION = "fn"
+	LET      = "let"
+	RETURN   = "return"
+)
