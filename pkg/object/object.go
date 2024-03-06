a80118b3fd435b7380b19ec14d131f1a771665c2
samasno <emmanuel@dv-analytics.io>
2024-03-06 06:49:04 -0600

    added return object

--- pkg/object/object.go
+++ pkg/object/object.go
@@ -2,35 +2,42 @@ package object
 
 import "fmt"
 
-
 type Object interface {
-  Type() ObjectType
-  Inspect() string
+	Type() ObjectType
+	Inspect() string
 }
 
 type Integer struct {
-  Value int64
+	Value int64
 }
 
 type Boolean struct {
-  Value bool
+	Value bool
 }
 
-type Null struct {}
+type Return struct {
+	Value Object
+}
 
-func (i *Integer) Type() ObjectType {return INTEGER_OBJ}
-func (i *Integer) Inspect() string {return fmt.Sprintf("%d", i.Value)}
+type Null struct{}
 
-func (b *Boolean) Type() ObjectType {return BOOLEAN_OBJ}
-func (b *Boolean) Inspect() string {return fmt.Sprintf("%t", b.Value)}
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
-func (n *Null) Type() ObjectType {return NULL_OBJ}
-func (n *Null) Inspect() string {return fmt.Sprintf("%v", nil)}
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return fmt.Sprintf("%v", nil) }
+
+func (r *Return) Type() ObjectType { return RETURN_OBJ }
+func (r *Return) Inspect() string  { return r.Value.Inspect() }
 
 type ObjectType string
 
 const (
-  INTEGER_OBJ = "INTEGER"
-  BOOLEAN_OBJ = "BOOLEAN"
-  NULL_OBJ = "NULL"
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+	RETURN_OBJ  = "RETURN"
 )
