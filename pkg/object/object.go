816e9f221e1c225b7c244100e5c5ca24be344d4f
samasno <emmanuel@dv-analytics.io>
2024-03-11 14:49:55 -0500

    string object

--- pkg/object/object.go
+++ pkg/object/object.go
@@ -9,33 +9,33 @@ import (
 )
 
 func NewEnvironment() *Environment {
-  return &Environment{
-    store: map[string]Object{},
-  }
+	return &Environment{
+		store: map[string]Object{},
+	}
 }
 
 func NewEnclosedEnvironment(outer *Environment) *Environment {
-  return &Environment{store:map[string]Object{}, outer: outer}
+	return &Environment{store: map[string]Object{}, outer: outer}
 }
 
 type Environment struct {
-  store map[string]Object
-  outer *Environment
+	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
-  obj, ok := e.store[name]
+	obj, ok := e.store[name]
 
-  if !ok && e.outer != nil {
-    obj, ok = e.outer.Get(name)
-  }
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 
-  return obj, ok
+	return obj, ok
 }
 
-func(e *Environment) Set(name string, value Object) Object {
-  e.store[name] = value
-  return value
+func (e *Environment) Set(name string, value Object) Object {
+	e.store[name] = value
+	return value
 }
 
 type Object interface {
@@ -47,6 +47,10 @@ type Integer struct {
 	Value int64
 }
 
+type String struct {
+	Value string
+}
+
 type Boolean struct {
 	Value bool
 }
@@ -56,13 +60,13 @@ type Return struct {
 }
 
 type Error struct {
-  Message string 
+	Message string
 }
 
 type Function struct {
-  Params []*ast.Identifier
-  Body *ast.BlockStatement
-  Env *Environment
+	Params []*ast.Identifier
+	Body   *ast.BlockStatement
+	Env    *Environment
 }
 
 type Null struct{}
@@ -80,35 +84,39 @@ func (r *Return) Type() ObjectType { return RETURN_OBJ }
 func (r *Return) Inspect() string  { return r.Value.Inspect() }
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
-func (e *Error) Inspect() string { return e.Message }
+func (e *Error) Inspect() string  { return e.Message }
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
 
 func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
 func (f *Function) Inspect() string {
-  var out bytes.Buffer
-  params := []string{}
-  for _, p := range f.Params {
-    params = append(params, p.String())
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range f.Params {
+		params = append(params, p.String())
 
-  }
+	}
 
-  out.WriteString("fn")
-  out.WriteString("(")
-  out.WriteString(strings.Join(params, ","))
-  out.WriteString(") {\n")
-  out.WriteString(f.Body.String())
-  out.WriteString("\n}")
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ","))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
 
-  return out.String()
-  
+	return out.String()
+
 }
 
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
-	RETURN_OBJ  = "RETURN"
-  ERROR_OBJ = "ERROR"
-  FUNCTION_OBJ = "FUNCTION"
+	INTEGER_OBJ  = "INTEGER"
+	BOOLEAN_OBJ  = "BOOLEAN"
+	NULL_OBJ     = "NULL"
+	RETURN_OBJ   = "RETURN"
+	ERROR_OBJ    = "ERROR"
+	FUNCTION_OBJ = "FUNCTION"
+	STRING_OBJ   = "STRING"
 )
