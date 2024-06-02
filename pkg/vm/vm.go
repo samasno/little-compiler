556d1ef9d7311afe7d3474c06c7f1d77eaf5453a
samasno <samasno@samasno.com>
2024-06-02 17:14:19 -0500

    unstash hashes

---
+++ pkg/vm/vm.go
@@ -0,0 +1,357 @@
+package vm
+
+import (
+	"fmt"
+
+	"github.com/samasno/little-compiler/pkg/code"
+	"github.com/samasno/little-compiler/pkg/compiler"
+	"github.com/samasno/little-compiler/pkg/frontend/object"
+)
+
+const StackSize = 2048
+const GlobalSize = 65536
+
+var True = &object.Boolean{Value: true}
+var False = &object.Boolean{Value: false}
+var Null = &object.Null{}
+
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+	stack        []object.Object
+	sp           int
+  globals []object.Object
+}
+
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		instructions: bytecode.Instructions,
+		constants:    bytecode.Constants,
+		stack:        make([]object.Object, StackSize),
+		sp:           0,
+    globals: make([]object.Object, GlobalSize),
+	}
+}
+
+func NewWithGlobalStore(bytecode *compiler.Bytecode,store []object.Object) *VM {
+  vm := New(bytecode)
+  vm.globals = store
+  return vm
+}
+
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+
+		case code.OpAdd, code.OpSub, code.OpMul, code.OpDiv:
+			err := vm.executeBinaryOperation(op)
+
+			if err != nil {
+				return err
+			}
+
+		case code.OpPop:
+			vm.pop()
+
+		case code.OpTrue:
+			err := vm.push(True)
+			if err != nil {
+				return err
+			}
+
+		case code.OpFalse:
+			err := vm.push(False)
+			if err != nil {
+				return err
+			}
+
+    case code.OpNull:
+      err := vm.push(Null)
+      if err != nil {
+        return err
+      }
+
+		case code.OpGreaterThan, code.OpEqual, code.OpNotEqual:
+			err := vm.executeComparison(op)
+			if err != nil {
+				return err
+			}
+
+		case code.OpMinus:
+			err := vm.executeMinusOperator()
+			if err != nil {
+				return err
+			}
+
+		case code.OpBang:
+			err := vm.executeBangOperator()
+			if err != nil {
+				return err
+			}
+
+		case code.OpJump:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip = pos - 1
+
+		case code.OpJumpNotTruthy:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+
+			condition := vm.pop()
+
+			if !isTruthy(condition) {
+				ip = pos - 1
+			}
+
+    case code.OpSetGlobal: 
+      globalIndex := code.ReadUint16(vm.instructions[ip+1:])
+      ip += 2 
+      vm.globals[globalIndex] = vm.pop()
+	
+    case code.OpGetGlobal:
+      globalIndex := code.ReadUint16(vm.instructions[ip+1:])
+      ip += 2 
+      err := vm.push(vm.globals[globalIndex])
+      if err != nil {
+        return err
+      }
+      
+    case code.OpArray:
+      numElements := int(code.ReadUint16(vm.instructions[ip + 1:]))
+
+      ip += 2 
+
+      array := vm.buildArray(vm.sp - numElements, vm.sp)
+      
+      vm.sp = vm.sp - numElements
+
+      err := vm.push(array)
+      if err != nil {
+        return err
+      }
+
+    case code.OpHash:
+      numElements := int(code.ReadUint16(vm.instructions[ip + 1:]))
+
+      ip += 2
+
+      hash, err := vm.buildHash(vm.sp - numElements, vm.sp)
+      if err != nil {
+        return err
+      }
+
+      vm.sp = vm.sp - numElements
+
+      err = vm.push(hash)
+      if err != nil {
+        return err
+      }
+
+    }
+
+	}
+
+	return nil
+}
+
+func (vm *VM) buildHash(start, end int) (object.Object, error ) {
+  hashedPairs := map[object.HashKey]object.HashPair{}
+
+  for i := start; i < end; i+=2 {
+    key := vm.stack[i]
+    value := vm.stack[i+1]
+
+    pair := object.HashPair{Key: key, Value: value}
+
+    hashKey, ok := key.(object.Hashable)
+    if !ok {
+      return nil, fmt.Errorf("unusable hash key: %s", key.Type())
+    }
+    
+    hashedPairs[hashKey.HashKey()] = pair
+  }
+
+  return &object.Hash{Pairs: hashedPairs}, nil
+}
+
+func (vm *VM) buildArray(start, end int) object.Object {
+  elements := make([]object.Object, end - start)
+
+  for i := start;i < end; i++ {
+    elements[i-start] = vm.stack[i]
+  }
+
+  return &object.Array{Elements: elements}
+}
+
+func (vm *VM) executeBangOperator() error {
+	operand := vm.pop()
+	switch operand {
+	case True:
+		return vm.push(False)
+	case False:
+		return vm.push(True)
+  case Null:
+    return vm.push(True)
+	default:
+		return vm.push(False)
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj := obj.(type) {
+	case *object.Boolean:
+		return obj.Value
+  case *object.Null:
+    return false
+	default:
+		return true
+	}
+}
+
+func (vm *VM) executeMinusOperator() error {
+	operand := vm.pop()
+
+	if operand.Type() != object.INTEGER_OBJ {
+		return fmt.Errorf("unsupported type for negation: %s", operand.Type())
+	}
+
+	value := operand.(*object.Integer).Value
+	return vm.push(&object.Integer{Value: -value})
+}
+
+func (vm *VM) executeBinaryOperation(op code.Opcode) error {
+	right := vm.pop()
+	left := vm.pop()
+
+	leftType := left.Type()
+	rightType := right.Type()
+
+	switch {
+	case leftType == object.INTEGER_OBJ && rightType == object.INTEGER_OBJ:
+		return vm.executeBinaryIntegerOperation(op, left, right)
+  case leftType == object.STRING_OBJ && rightType == object.STRING_OBJ:
+    return vm.executeBinaryStringOperation(op, left, right)
+  default:
+  	return fmt.Errorf("unsupported types for binary operation: %s %s", leftType, rightType)
+  }
+}
+  
+func (vm *VM) executeBinaryStringOperation(op code.Opcode, left, right object.Object) error {
+  leftValue := left.(*object.String).Value
+  rightValue := right.(*object.String).Value
+
+  var result string
+
+  switch op {
+  case code.OpAdd:
+    result = leftValue + rightValue
+  default:
+    return fmt.Errorf("unsupported string operation: %d", op)
+  }
+
+  return vm.push(&object.String{Value:result})
+}
+func (vm *VM) executeBinaryIntegerOperation(op code.Opcode, left, right object.Object) error {
+	leftValue := left.(*object.Integer).Value
+	rightValue := right.(*object.Integer).Value
+
+	var result int64
+
+	switch op {
+	case code.OpAdd:
+		result = leftValue + rightValue
+	case code.OpSub:
+		result = leftValue - rightValue
+	case code.OpMul:
+		result = leftValue * rightValue
+	case code.OpDiv:
+		result = leftValue / rightValue
+	default:
+		return fmt.Errorf("unsupported integer operation: %d", op)
+	}
+
+	return vm.push(&object.Integer{Value: result})
+}
+
+func (vm *VM) executeComparison(op code.Opcode) error {
+	right := vm.pop()
+	left := vm.pop()
+
+	if left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ {
+		return vm.executeIntegerComparison(op, left, right)
+	}
+
+	switch op {
+	case code.OpEqual:
+		return vm.push(nativeBoolToBooleanObject(right == left))
+	case code.OpNotEqual:
+		return vm.push(nativeBoolToBooleanObject(right != left))
+	default:
+		return fmt.Errorf("unsupported comparison operator: %d", op)
+	}
+}
+
+func (vm *VM) executeIntegerComparison(op code.Opcode, left, right object.Object) error {
+	leftValue := left.(*object.Integer).Value
+	rightValue := right.(*object.Integer).Value
+
+	switch op {
+	case code.OpEqual:
+		return vm.push(nativeBoolToBooleanObject(leftValue == rightValue))
+	case code.OpNotEqual:
+		return vm.push(nativeBoolToBooleanObject(leftValue != rightValue))
+	case code.OpGreaterThan:
+		return vm.push(nativeBoolToBooleanObject(leftValue > rightValue))
+
+	default:
+		return fmt.Errorf("unknown operator: %d", op)
+	}
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return True
+	}
+
+	return False
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+
+	vm.stack[vm.sp] = o
+	vm.sp++
+
+	return nil
+}
+
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+
+	return vm.stack[vm.sp-1]
+}
+
+func (vm *VM) LastPoppedStackElement() object.Object {
+	return vm.stack[vm.sp]
+}
+
+func (vm *VM) pop() object.Object {
+	o := vm.stack[vm.sp-1]
+	vm.sp--
+
+	return o
+}
---
+++ pkg/vm/vm_test.go
@@ -0,0 +1,250 @@
+package vm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/samasno/little-compiler/pkg/compiler"
+	"github.com/samasno/little-compiler/pkg/frontend/ast"
+	"github.com/samasno/little-compiler/pkg/frontend/lexer"
+	"github.com/samasno/little-compiler/pkg/frontend/object"
+	"github.com/samasno/little-compiler/pkg/frontend/parser"
+)
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1+2", 3},
+		{"7+7", 14},
+		{"3*9", 27},
+		{"3/1", 3},
+		{"100-50", 50},
+		{"-10", -10},
+	}
+
+	runVmTests(t, tests)
+}
+
+func TestBooleanExpressions(t *testing.T) {
+	tests := []vmTestCase{
+		{"true", true},
+		{"false", false},
+		{"true == true", true},
+		{"false == false", true},
+		{"false != true", true},
+		{"true != false", true},
+		{"10 < 100", true},
+		{"100 < 10", false},
+		{"100 > 10", true},
+		{"10 > 100", false},
+		{"10 != 100", true}, //break
+		{"10 == 10", true},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
+		{"!true", false},
+		{"!false", true},
+		{"!(1 < 3)", false},
+		{"!!true", true},
+		{"!5", false},
+    {"!(if(false){ 5;})", true},
+	}
+
+	runVmTests(t, tests)
+}
+
+func TestConditionals(t *testing.T) {
+	tests := []vmTestCase{
+		{"if (true) { 10} ", 10},
+		{"if (true) { 10} else { 20 }", 10},
+		{"if (false) { 10} else { 20 }", 20},
+		{"if (1) { 10} ", 10},
+		{"if (1 < 2) { 10} ", 10},
+		{"if (1 < 2) { 10} else { 20 } ", 10},
+		{"if (1 > 2) { 10} else { 20 }", 20},
+		{"if (false) {1}", Null},
+		{"if (1 > 2) {1}", Null},
+	  {"if((if(true){10})) {10} else {20}", 10},
+    {"if((if(false){10})) {10} else {20}", 20},
+  }
+
+	runVmTests(t, tests)
+}
+
+func TestGlobalLetStatements(t *testing.T) {
+  tests := []vmTestCase{
+    {`let one = 1; one;`, 1},
+    {`let one = 1; let two = 2; one + two`, 3},
+    {`let one = 1; let two = one + one; one + two;`, 3},
+  }
+
+  runVmTests(t, tests)
+}
+
+func TestStringExpression(t *testing.T) {
+  tests := []vmTestCase {
+    {`"monkey"`, "monkey"},
+    {`"test " + "works"`, "test works"},
+  }
+
+  runVmTests(t, tests)
+}
+
+func TestArrayLiterals(t *testing.T) {
+  tests := []vmTestCase {
+    {`["test""]`, []string{"test"}},
+    {`[]`, []int{}},
+    {`[1+2, 4, 5]`, []int{3,4,5}},
+  }
+
+  runVmTests(t,tests)
+}
+
+func TestHashLiterals(t *testing.T) {
+  tests := []vmTestCase {
+    {`{}`, map[object.HashKey]int64{}},
+    {`{1:2, 3:4}`, map[object.HashKey]int64 {
+      (&object.Integer{Value: 1}).HashKey(): 2,
+      (&object.Integer{Value: 3}).HashKey(): 4},
+    },
+    {`{2 * 2: 10 / 2,}`, map[object.HashKey]int64 {
+      (&object.Integer{Value: 4}).HashKey(): 5},
+    }, 
+  }
+
+  runVmTests(t, tests)
+}
+
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+		if err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+  case string:
+    err := testStringObject(expected, actual)
+    if err != nil {
+      t.Errorf("testStringObject failed: %s", err)
+    }
+	case bool:
+		err := testBooleanObject(expected, actual)
+		if err != nil {
+			t.Errorf("testBooleanObject failed: %s", err.Error())
+		}
+	case *object.Null:
+		if actual != Null {
+			t.Errorf("testBooleanObject failed: expected Null got %T (%+v)", actual, actual)
+		}
+  case []int:
+    actual, ok := actual.(*object.Array)
+    if !ok {
+      t.Errorf("testArrayObject failed: not array got %T (+%v)", actual, actual)
+    }
+
+    if len(expected) != len(actual.Elements) {
+      t.Errorf("wrong length of array: want %d got %d", len(expected), len(actual.Elements))
+    }
+
+    for i, el := range expected {
+      err := testIntegerObject(int64(el), actual.Elements[i])  
+      if err != nil {
+        t.Errorf("testIntegerObject failed: %s", err)
+      }
+    }
+  case map[object.HashKey]int64:
+    hash, ok := actual.(*object.Hash)
+    if !ok {
+      t.Errorf("object not Hash, got %T (%+v)", actual, actual)
+      return
+    }
+
+    if len(hash.Pairs) != len(expected) {
+      t.Errorf("hash has wrong number of pairs. want %d got %d", len(expected), len(hash.Pairs))
+      return
+    }
+
+    for expectedKey, expectedValue := range expected {
+      pair, ok := hash.Pairs[expectedKey]
+      if !ok {
+        t.Errorf("hash does not contain value for key %v", expectedKey)
+      }
+
+      err := testIntegerObject(expectedValue, pair.Value)
+      if err != nil {
+        t.Errorf("testIntegerObject failed: %s", err)
+      }
+    }
+	}
+}
+
+func testBooleanObject(expected bool, obj object.Object) error {
+	v := obj.(*object.Boolean).Value
+	if v != expected {
+		return fmt.Errorf("expected %v got %v", expected, v)
+	}
+	return nil
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+
+		comp := compiler.New()
+
+		err := comp.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error:%s", err)
+		}
+
+		vm := New(comp.Bytecode())
+
+		err = vm.Run()
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+
+		stackElem := vm.LastPoppedStackElement()
+		testExpectedObject(t, tt.expected, stackElem)
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got %T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. want %d got %d", expected, result.Value)
+	}
+
+	return nil
+}
+
+func testStringObject(expected string, actual object.Object) error {
+  result, ok := actual.(*object.String)
+  if !ok {
+    return fmt.Errorf("object is not string, got %T (%+v)", actual, actual)
+  }
+
+  if result.Value != expected {
+    return fmt.Errorf("object has wrong value. want %s got %s", expected, result.Value)
+  }
+
+  return nil
+}
+
+type vmTestCase struct {
+	input    string
+	expected interface{}
+}
